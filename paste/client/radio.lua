f2a9f472339bb08147da18fe7db80bb5a2dd90a7
Fly <108137356+prvtfly@users.noreply.github.com>
2023-02-10 16:18:39 +0100

    Update radio.lua

--- paste/client/radio.lua
+++ paste/client/radio.lua
@@ -1,11 +1,4 @@
-local ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+ESX = exports['es_extended']:getSharedObject();
 
 local funkTask = nil
 local currentFunk = ""
