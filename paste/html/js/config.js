e3ca185179d23bfd0da22b2b556560022871152f
Kuuzoo <nilscosmo@hotmail.com>
2023-08-02 20:17:52 +0200

    Cleaning or something, if not working get the last one

--- paste/html/js/config.js
+++ paste/html/js/config.js
@@ -1,7 +1,8 @@
+// Paste this code in the config.js under the "Applications" section
 Config.Applications = {
     "Radio": {
         label: "Radio",
-        description: "App zum Verbinden mit Funkkanälen.",
+        description: "App to join radio channels",
         icon: "fas fa-broadcast-tower",
         icon_color: "white",
         icon_image: "",
@@ -10,7 +11,6 @@ Config.Applications = {
         bottom: false,
         downloadable: true,
         downloadTime: 1500, // in miliseconds, 1000 ms = 1 second
-        category: "default",
         notifications: {
             icon: "fas fa-broadcast-tower",
             color: "#fff"
--- paste/html/js/radio.js
+++ paste/html/js/radio.js
@@ -1,42 +1,28 @@
+// Paste this file into "html/js/radio.js"
+
 $('#setFunk').hide();
 $('#disableFunk').hide();
 
-ESX.Phone.functions.addButton("#setFunk", function(element, event) {
+HR.Phone.functions.addButton("#setFunk", function(element, event) {
   if ($("#funkkanal").val()) {
     $.post(`https://${GetParentResourceName()}/SetFunk`, JSON.stringify({ channel: $('#funkkanal').val() }));
-    addverlauf($('#funkkanal').val())
-    ESX.Phone.functions.sendNotification("fas fa-microphone", "#fff", "Radio", "Du hast den Funkkanal "+$('#funkkanal').val()+" MHz betreten", 3000)
+    HR.Phone.functions.sendNotification("fas fa-microphone", "#fff", "Radio", "Du hast den Funkkanal " + $('#funkkanal').val() + " MHz betreten", 3000)
     $('#setFunk').fadeOut();
     $('#disableFunk').fadeIn();
   }
 })
 
-ESX.Phone.functions.addButton("#disableFunk", function(element, event) {
+HR.Phone.functions.addButton("#disableFunk", function(element, event) {
   $.post(`https://${GetParentResourceName()}/DisableFunk`, JSON.stringify({ channel: $('#funkkanal').val() }));
-  addverlauf($('#funkkanal').val())
-  ESX.Phone.functions.sendNotification("fas fa-microphone", "#fff", "Radio", "Du hast den Funkkanal "+$('#funkkanal').val()+" MHz verlassen", 3000)
+  HR.Phone.functions.sendNotification("fas fa-microphone", "#fff", "Radio", "Du hast den Funkkanal " + $('#funkkanal').val() + " MHz verlassen", 3000)
   $('#setFunk').fadeIn();
   $('#disableFunk').fadeOut();
 })
 
-$('#funkkanal').on('keyup', function() {
+$('#funkkanal').on('keyup', function () {
   if ($("#funkkanal").val()) {
     $('#setFunk').fadeIn();
   } else {
     $('#setFunk').fadeOut();
   }
 });
-
-function connect(channel) {
-  $.post(`https://${GetParentResourceName()}/SetFunk`, JSON.stringify({ channel: channel }));
-}
-
-function addverlauf(channel) {
-  $('.scroll__container').prepend
-  (`
-  <div class="scroll__item" onclick="connect2('`+ channel +`')">
-      <i class="fas fa-wifi"></i>
-      <p><span>ID:</span> `+ channel +`</p>
-  </div>
-  `);
-}
